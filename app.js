11430f528c0f28e2d5b86c7c0b1ea32857e398a9
Parv Nautiyal <parvnautiyal4@gmail.com>
2022-12-04 13:43:17 +0530

    fixes

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-// app.engine('handlebars', expressHbs({layoutsDir:'views/layouts/', defaultLayout: 'main-layout'}));
-// app.set('view engine', 'handlebars');
-//app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
